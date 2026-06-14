05ea66e0c80e04dae4c15793b9c0e070cda92d95
Mohammad <m.yousef55766@gmail.com>
2026-06-14 21:31:36 +0300

    testing the live preview

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Nidal-Refractories/' : '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+}))
